93b872976acb45398d5e99ab59dc16e9d006fae6
nregueira <regueira2010@gmail.com>
2026-04-10 18:52:21 -0400

    feat(ex01): implementar clase Alumno y lógica de vinculación con el DOM

---
+++ assets/ejercicios/EJERCICIO01/assets/js/app.js
@@ -0,0 +1,50 @@
+const inputNombre = document.getElementById("inputNombreAlumno");
+const inputEdad = document.getElementById("inputEdadAlumno");
+const inputCarrera = document.getElementById("inputCarreraAlumno");
+const btnIniciar = document.getElementById("btnIniciar");
+const respDOM = document.getElementById("resultado");
+
+class Alumno {
+  constructor(nombre, edad, carrera) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.carrera = carrera;
+  }
+
+  saludar(contenedor) {
+    contenedor.classList.remove("d-none");
+    contenedor.innerHTML = `
+      <p cvlass="mb-0">
+      <strong>Hola</strong> soy <b>${this.nombre}</b>, 
+      tengo <b>${this.edad}</b> años y 
+      estoy cursando la carrera de <b>${this.carrera}</b>.
+      </p>
+    `;
+
+    console.log(
+      `Hola, soy ${this.nombre},  tengo ${this.edad} años y estoy cursando la carrera de ${this.carrera}`,
+    );
+  }
+}
+
+btnIniciar.addEventListener("click", () => {
+  datoNombre = inputNombre.value;
+  datoEdad = inputEdad.value;
+  datoCarrera = inputCarrera.value;
+
+  if (!datoNombre || !datoEdad || !datoCarrera) {
+    respDOM.classList.remove("d-none");
+    respDOM.innerHTML = `
+        <div class="text-danger small fw-bold p-2 border border-danger rounded bg-light text-center">
+            ⚠️ Todos los campos son obligatorios
+        </div>
+    `;
+    return;
+  }
+  const alumno = new Alumno(datoNombre, datoEdad, datoCarrera);
+  alumno.saludar(respDOM);
+
+  inputNombre.value = "";
+  inputEdad.value = "";
+  inputCarrera.value = "";
+});
